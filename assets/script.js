be09f0c805f7c2dbb7d4e08cd200ea0224f57d10
Herman <evanaherman@gmail.com>
2022-12-22 14:44:27 -0800

    added pseudocode to js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 // Assignment Code
+
+// Global Variables
+var specialCharacters = [];
+var numericCharacters = [];
+var lowerCasedCharacters = [];
+var uppderCasedCharacters = [];
+
+var length;
+var result = "";
+var availableChars = []
+
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +25,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // own function?
+  // prompt for length of the password
+  // check if valid length, if wrong prompt again
+
+  // ask for different types of characters
+  // 4 confirms for each type
+  // validate they selected at least one
+
+  // separate function
+  // iterate length number of times
+  // select random char from available char
+  // add to result
+
+
+
+
+
+
+  return result
+}
+
